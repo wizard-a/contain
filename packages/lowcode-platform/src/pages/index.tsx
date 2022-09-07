55ffb65a5a5bf23d2ef59aac1b022e9146440959
jiechud <qiaojienet@163.com>
2022-09-07 23:13:03 +0800

    feat: 集成lowcode引擎

--- packages/lowcode-platform/src/pages/index.tsx
+++ packages/lowcode-platform/src/pages/index.tsx
@@ -1,15 +1,10 @@
 import yayJpg from '../assets/yay.jpg';
+import LowcodeEditor from '@/components/lowcode-editor';
 
 export default function HomePage() {
   return (
-    <div>
-      <h2>Yay! Welcome to umi!</h2>
-      <p>
-        <img src={yayJpg} width="388" />
-      </p>
-      <p>
-        To get started, edit <code>pages/index.tsx</code> and save to reload.
-      </p>
+    <div style={{ height: '100%' }}>
+      <LowcodeEditor />
     </div>
   );
 }
