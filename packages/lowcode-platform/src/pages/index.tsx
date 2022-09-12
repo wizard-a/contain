7aef493a65a488d6bfbe8935783b7eb053825341
jiechud <qiaojienet@163.com>
2022-09-12 14:55:06 +0800

    chore: 更改配置

--- packages/lowcode-platform/src/pages/index.tsx
+++ packages/lowcode-platform/src/pages/index.tsx
@@ -1,5 +1,7 @@
-import yayJpg from '../assets/yay.jpg';
-import LowcodeEditor from '@/components/lowcode-editor';
+import React from 'react';
+// import yayJpg from '../assets/yay.jpg';
+import LowcodeEditor from '../components/lowcode-editor';
+import LE from "@/components/lowcode-editor";
 
 export default function HomePage() {
   return (
--- packages/ui-material/README.md
+++ packages/ui-material/README.md
@@ -15,3 +15,10 @@ npm run lowcode:dev             # 打开开发环境，运行所有组件
 npm run lowcode:build
 ```
 
+*  等Volta支持pnpm 还原
+"husky": {
+    "hooks": {
+      "pre-commit": "f2elint commit-file-scan"
+    }
+  },
+
